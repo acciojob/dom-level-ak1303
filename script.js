153742d9be7c8d0e5cfd84f82b21472eb02d2462
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-28 01:40:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let list = document.querySelectorAll('li');
+let count=0;
+for (let i = 0; i < list.length; i++) {
+  if (list[i].getAttribute('id') === 'level') {
+    break;
+  }
+  count++;
+}
+alert(`The level of the element is: ${count}`);   
